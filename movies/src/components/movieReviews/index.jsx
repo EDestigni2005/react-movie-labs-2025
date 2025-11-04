0e774725ac13ba41b7aaf14d0a114c120c3a629c
Elijah <20107406@wit.ie>
2025-11-04 03:45:17 +0000

    Updating move page to use react query

--- movies/src/components/movieReviews/index.jsx
+++ movies/src/components/movieReviews/index.jsx
@@ -6,19 +6,29 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
-import { Link } from "react-router-dom";
+import { Link } from "react-router";
 import { getMovieReviews } from "../../api/tmdb-api";
 import { excerpt } from "../../util";
+import { useQuery } from "@tanstack/react-query";
+import Spinner from '../spinner'
+
 
 export default function MovieReviews({ movie }) {
-    const [reviews, setReviews] = useState([]);
+    const { data, error, isPending, isError } = useQuery({
+        queryKey: ['reviews', { id: movie.id }],
+        queryFn: getMovieReviews,
+    });
 
-    useEffect(() => {
-        getMovieReviews(movie.id).then((reviews) => {
-            setReviews(reviews);
-        });
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+    if (isPending) {
+        return <Spinner />;
+    }
+
+    if (isError) {
+        return <h1>{error.message}</h1>;
+    }
+
+    const reviews = data.results;
+
 
     return (
         <TableContainer component={Paper}>
--- movies/src/components/templateMoviePage/index.jsx
+++ movies/src/components/templateMoviePage/index.jsx
@@ -4,52 +4,63 @@ import Grid from "@mui/material/Grid";
 import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
 import { getMovieImages } from "../../api/tmdb-api";
+import { useQuery } from "@tanstack/react-query";
+import Spinner from '../spinner'
+
 
 const TemplateMoviePage = ({ movie, children }) => {
-  const [images, setImages] = useState([]);
-
-  useEffect(() => {
-    getMovieImages(movie.id).then((images) => {
-      setImages(images);
+    const { data, error, isPending, isError } = useQuery({
+        queryKey: ['images', { id: movie.id }],
+        queryFn: getMovieImages,
     });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
 
-  return (
-    <>
-      <MovieHeader movie={movie} />
+    if (isPending) {
+        return <Spinner />;
+    }
 
-      <Grid container spacing={5} style={{ padding: "15px" }}>
-        <Grid size={{xs: 3}}>
-          <div sx={{
-            display: "flex",
-            flexWrap: "wrap",
-            justifyContent: "space-around",
-          }}>
-            <ImageList
-                sx={{
-                    height: "100vh",
-                }}
-                cols={1}
-            >
-                {images.map((image) => (
-                    <ImageListItem key={image.file_path} cols={1}>
-                    <img
-                        src={`https://image.tmdb.org/t/p/w500/${image.file_path}`}
-                        alt={image.poster_path}
-                    />
-                    </ImageListItem>
-                ))}
-            </ImageList>
-          </div>
-        </Grid>
+    if (isError) {
+        return <h1>{error.message}</h1>;
+    }
 
-        <Grid size={{xs: 9}}>
-          {children}
-        </Grid>
-      </Grid>
-    </>
-  );
+    const images = data.posters
+
+
+
+    return (
+        <>
+            <MovieHeader movie={movie} />
+
+            <Grid container spacing={5} style={{ padding: "15px" }}>
+                <Grid size={{ xs: 3 }}>
+                    <div sx={{
+                        display: "flex",
+                        flexWrap: "wrap",
+                        justifyContent: "space-around",
+                    }}>
+                        <ImageList
+                            sx={{
+                                height: "100vh",
+                            }}
+                            cols={1}
+                        >
+                            {images.map((image) => (
+                                <ImageListItem key={image.file_path} cols={1}>
+                                    <img
+                                        src={`https://image.tmdb.org/t/p/w500/${image.file_path}`}
+                                        alt={image.poster_path}
+                                    />
+                                </ImageListItem>
+                            ))}
+                        </ImageList>
+                    </div>
+                </Grid>
+
+                <Grid size={{ xs: 9 }}>
+                    {children}
+                </Grid>
+            </Grid>
+        </>
+    );
 };
 
 export default TemplateMoviePage;

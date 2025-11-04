a023b0c40258d0c9a65b17cad219f60fb4eb0039
Elijah <20107406@wit.ie>
2025-11-04 02:20:59 +0000

    Refactor use movie custom hook

--- movies/src/pages/movieDetailsPage.jsx
+++ movies/src/pages/movieDetailsPage.jsx
@@ -1,26 +1,20 @@
-import React, {useState, useEffect}  from "react";
-import { useParams } from 'react-router-dom';
+import React from "react";
+import { useParams } from 'react-router';
 import MovieDetails from "../components/movieDetails/";
-import TemplateMoviePage from "../components/templateMoviePage";
-import { getMovie } from "../api/tmdb-api";
+import PageTemplate from "../components/templateMoviePage";
+import useMovie from "../hooks/useMovie";
 
 const MoviePage = (props) => {
   const { id } = useParams();
-  const [movie, setMovie] = useState(null);
-
-  useEffect(() => {
-    getMovie(id).then((movie) => {
-      setMovie(movie);
-    });
-  }, [id]);
+  const [movie] = useMovie(id);
 
   return (
     <>
       {movie ? (
         <>
-          <TemplateMoviePage movie={movie}>
+          <PageTemplate movie={movie}>
             <MovieDetails movie={movie} />
-          </TemplateMoviePage>
+          </PageTemplate>
         </>
       ) : (
         <p>Waiting for movie details</p>

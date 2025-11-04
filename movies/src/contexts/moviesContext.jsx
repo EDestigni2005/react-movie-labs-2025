105ba620abdbee1a1ef5b7172c2dff9064c8ce07
Elijah <20107406@wit.ie>
2025-11-04 16:24:59 +0000

    Tagging upcoming moves as must watch ompleted movie lab

--- movies/src/contexts/moviesContext.jsx
+++ movies/src/contexts/moviesContext.jsx
@@ -5,6 +5,7 @@ export const MoviesContext = React.createContext(null);
 const MoviesContextProvider = (props) => {
     const [favorites, setFavorites] = useState([])
     const [myReviews, setMyReviews] = useState({})
+    const [mustWatch, setMustWatch] = useState([]);
 
 
     const addToFavorites = (movie) => {
@@ -28,7 +29,17 @@ const MoviesContextProvider = (props) => {
     const addReview = (movie, review) => {
         setMyReviews({ ...myReviews, [movie.id]: review })
     };
-    //console.log(myReviews);
+
+    const addToMustWatch = (movie) => {
+        let newMustWatch = [];
+        if (!mustWatch.includes(movie.id)) {
+            newMustWatch = [...mustWatch, movie.id];
+        } else {
+            newMustWatch = [...mustWatch];
+        }
+        setMustWatch(newMustWatch);
+        console.log(newMustWatch);
+    };
 
 
     return (
@@ -38,6 +49,8 @@ const MoviesContextProvider = (props) => {
                 addToFavorites,
                 removeFromFavorites,
                 addReview,
+                mustWatch,
+                addToMustWatch
             }}
         >
             {props.children}
--- movies/src/pages/upcomingMoviesPage.jsx
+++ movies/src/pages/upcomingMoviesPage.jsx
@@ -1,23 +1,30 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext } from "react";
+import { useQuery } from "@tanstack/react-query";
 import PageTemplate from "../components/templateMovieListPage";
 import { getUpcomingMovies } from "../api/tmdb-api";
 import PlaylistAddIcon from "@mui/icons-material/PlaylistAdd";
+import { MoviesContext } from "../contexts/moviesContext";
 
 const UpcomingMoviesPage = () => {
-  const [movies, setMovies] = useState([]);
+  const { data: movies = [] } = useQuery({
+    queryKey: ["upcoming"],
+    queryFn: getUpcomingMovies,
+  });
 
-  useEffect(() => {
-    getUpcomingMovies()
-      .then((m) => setMovies(Array.isArray(m) ? m : []))
-      .catch(() => setMovies([]));
-  }, []);
+  const { addToMustWatch = () => {} } = useContext(MoviesContext) || {};
 
   return (
     <PageTemplate
       title="Upcoming Movies"
-      movies={movies}
-      selectFavorite={() => {}}  
-      action={() => <PlaylistAddIcon color="primary" />}
+      movies={Array.isArray(movies) ? movies : []}
+      selectFavorite={() => {}}
+      action={(movie) => (
+        <PlaylistAddIcon
+          color="primary"
+          onClick={() => addToMustWatch(movie)}
+          style={{ cursor: "pointer" }}
+        />
+      )}
     />
   );
 };

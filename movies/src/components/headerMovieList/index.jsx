60674c79cff40cb2b83df1d9a011b38a168fd69c
Elijah <20107406@wit.ie>
2025-11-04 02:40:43 +0000

    Fix backward/forward arrows

--- movies/src/components/headerMovieList/index.jsx
+++ movies/src/components/headerMovieList/index.jsx
@@ -5,34 +5,32 @@ import Paper from "@mui/material/Paper";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 
-const Header = (props ) => {
-  const title = props.title
-  return (
-    <Paper 
-      component="div" 
-      sx={{
-        display: "flex",
-        justifyContent: "space-around",
-        flexWrap: "wrap",
-        marginBottom: 1.5,
-      }}
-      >
-      <IconButton
-        aria-label="go back"
-      >
-        <ArrowBackIcon color="primary" fontSize="large" />
-      </IconButton>
+const Header = (props) => {
+    const title = props.title
+    return (
+        <Paper
+            component="div"
+            sx={{
+                display: "flex",
+                justifyContent: "space-around",
+                flexWrap: "wrap",
+                marginBottom: 1.5,
+            }}
+        >
+            <IconButton aria-label="go back" onClick={() => navigate(-1)}>
+                <ArrowBackIcon color="primary" fontSize="large" />
+            </IconButton>
 
-      <Typography variant="h4" component="h3">
-        {title}
-      </Typography>
-      <IconButton
-        aria-label="go forward"
-      >
-        <ArrowForwardIcon color="primary" fontSize="large" />
-      </IconButton>
-    </Paper>
-  );
+
+            <Typography variant="h4" component="h3">
+                {title}
+            </Typography>
+            <IconButton aria-label="go forward" onClick={() => navigate(+1)}>
+                <ArrowForwardIcon color="primary" fontSize="large" />
+            </IconButton>
+
+        </Paper>
+    );
 };
 
 export default Header;
